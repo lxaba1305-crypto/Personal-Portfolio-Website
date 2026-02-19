0dc645ee3ad52d5cf5068622006b219976c2818d
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-02-19 20:28:30 +0200

    Added imports to App.jsx and edited skills section

--- Personal-Portfolio-Website/src/App.jsx
+++ Personal-Portfolio-Website/src/App.jsx
@@ -1,1 +1,29 @@
 import React from 'react'
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Journey from './components/Journey'
+import Skills from './components/Skills'
+import Vision from './components/Vision'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Journey />
+      <Skills />
+      <Vision />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
